738e0d9dc97e789196c75d8b7b45dad9998f1514
Pedro <antonioaren@hotmail.com>
2018-04-02 12:36:36 +0100

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+function initMap() {
+    var uluru = { lat: 28.106368, lng: -15.445752 };
+    /* 28.106368, -15.445752 */
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+
+function myFunction() {    
+    document.getElementById("myDropdown1").classList.toggle("show");    
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+
 
 
